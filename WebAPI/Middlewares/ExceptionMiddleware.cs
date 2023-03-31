0c8006898ea58d0e6cfac92d9c576b3df4de345d
RumenWZ <98177567+RumenWZ@users.noreply.github.com>
2023-03-31 16:36:14 +0300

    web api calls return json error message

--- WebAPI/Middlewares/ExceptionMiddleware.cs
+++ WebAPI/Middlewares/ExceptionMiddleware.cs
@@ -1,13 +1,21 @@
-﻿namespace WebAPI.Middlewares
+﻿using System.Net;
+using WebAPI.Errors;
+
+namespace WebAPI.Middlewares
 {
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate next;
         private readonly ILogger<ExceptionMiddleware> logger;
-        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        private readonly IHostEnvironment env;
+        public ExceptionMiddleware(
+            RequestDelegate next, 
+            ILogger<ExceptionMiddleware> logger,
+            IHostEnvironment env)
         {
             this.next = next;
             this.logger = logger;
+            this.env = env;
         }
 
         public async Task Invoke(HttpContext context)
@@ -16,9 +24,20 @@
             {
                 await next(context);
             } catch (Exception ex) {
+                ApiError response;
+                HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+                if (env.IsDevelopment())
+                {
+                    response = new ApiError((int)statusCode, ex.Message, ex.StackTrace.ToString());
+                } else
+                {
+                    response = new ApiError((int)statusCode, ex.Message);
+                }
+
                 logger.LogError(ex, ex.Message);
-                context.Response.StatusCode = 500;
-                await context.Response.WriteAsync(ex.Message);
+                context.Response.StatusCode = (int)statusCode;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(response.ToString());
             }
 
         }
